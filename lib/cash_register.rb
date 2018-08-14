b7cd17b16fea68f15cd9d2018a02c2a1635887de
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-14 22:54:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   attr_accessor :total, :discount
   def initialize(total = 0, discount = 20)
     @total = total
-    @discount = discount.to_f / 100
+    @discount = discount
   end
 
 
