7ad21399a39807f375872748fdfbc4b9a099b747
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-14 22:44:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,11 +8,12 @@ class CashRegister
 
 
   def add_item(item, price, quantity = 1)
-    self.total += price * quantity
+    total += price * quantity
   end
 
   def apply_discount
     add_item(item, price, quantity)
+
   end
 
 
