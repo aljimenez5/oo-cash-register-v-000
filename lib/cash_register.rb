c5d226e01c6fd2582e91f1e5ab6ed43f9109b7a8
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-15 00:40:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :transaction
 
   def initialize(discount = 20, total = 0)
     @total = total
