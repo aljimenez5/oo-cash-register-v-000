efd36fa4c91844756484bed95b3b2f5f0834b5aa
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-15 01:33:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = 1)
-    @items << (quantity, item)
+    @items.push(quantity, item)
     @transactions << [item, price, quantity]
     self.total += price * quantity
   end
