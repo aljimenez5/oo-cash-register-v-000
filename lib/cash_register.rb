7bcc6bff6ff082cf39d517a8708633d0bc320284
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-15 00:01:58 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     self.total = price * quantity
     self.items = []
-    @items << item
+    items << item
     binding.pry
   end
 
