c0e1bac78a31f1fdcb0dc8ae96c3d9fec31aee64
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-15 00:41:11 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total, :discount, :items, :transaction
+  attr_accessor :total, :discount, :items, :transactions
 
   def initialize(discount = 20, total = 0)
     @total = total
@@ -21,6 +21,8 @@ class CashRegister
     items = []
     add_item(item, price).map{|item| items << item}
   end
+
+  def transactions
 
   def void_last_transaction
     self.add_item(item, price)
