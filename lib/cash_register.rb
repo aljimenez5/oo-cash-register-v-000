e9b3d571900a6657f0e72c8efd20a5c60b17fa3f
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-15 00:44:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
   def items
     @items = []
     @transactions << add_item(item, price, quantity = 1)
-    add_item(item, price, quantity = 1).map{|item, price, quantity| @transactions << item}
+    add_item(item, price, quantity = 1).map{|item, price, quantity| @items << item}
   end
 
   def transactions=(transactions)
